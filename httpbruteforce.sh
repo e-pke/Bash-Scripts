1cb3e0959f92b1bf28d2db4d3f7d5c0c97bfc914
Єpke <gujerke@gmail.com>
2026-02-04 15:52:39 -0300

    Update httpbruteforce.sh

--- httpbruteforce.sh
+++ httpbruteforce.sh
@@ -1,76 +1,100 @@
 #!/bin/bash
 # https://github.com/e-pke/Bash-Scripts
 
-testar() { printf "\r      ▶▶▶ TESTANDO:  %-50s" "$1" >&2; }
-limpar() { printf "\r\033[K" >&2; }
+# Estilos
+BLD="\033[1m"
+RST="\033[0m"
 
-echo -e "\n\033[1;39m"
-clear
-cat << EOF
-  ┌────────────────────────────────┐
-  │        HTTP BRUTE FORCE        │
-  └────────────────────────────────┘
+# Cores
+RED="\033[31m"
+BLU="\033[36m"
+STD="\033[39m"
 
-       ▄▄▄▄▄▄       ▄▄▄▄      by 
-      ██▀▀▀▀▀█       ██          
-      ██▄▄▄▄ ▀ ██▀█▄ ██ ▄█▀▄█▀█▄ 
-      ██▀▀▀▀ ▄ ██ ██ ████  ██▄█▀ 
-      ▀██████▀ ██▄█▀▄██ ▀█▄▀█▄▄■ 
-     ▄▄▄▄▄▄▄▄▄ ██ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-               ▀▀                
+# Função para mostrar o que está sendo testado
+testar() {
+	printf "\r      ▶▶▶ TESTANDO:  %-50s" "$1" >&2
+}
+
+# Função para limpar a linha após o teste
+limpar() {
+	printf "\r\033[K" >&2
+}
+
+# Função para encerrar o script mesmo com Ctrl+C
+encerrar() {
+	echo -e "\n\n${BLD}${RED}   ▶▶▶ ENCERRADO ${BLD}${BLU}"
+}
+trap encerrar exit
+
+# Função cabeçalho
+header() {
+	echo -e "\n${BLD}${STD}"
+	cat <<- EOF
+	  ┌────────────────────────────────┐
+	  │        HTTP BRUTE FORCE        │
+	  └────────────────────────────────┘
+	
+	       ▄▄▄▄▄▄       ▄▄▄▄      by 
+	      ██▀▀▀▀▀█       ██          
+	      ██▄▄▄▄ ▀ ██▀█▄ ██ ▄█▀▄█▀█▄ 
+	      ██▀▀▀▀ ▄ ██ ██ ████  ██▄█▀ 
+	      ▀██████▀ ██▄█▀▄██ ▀█▄▀█▄▄■ 
+	     ▄▄▄▄▄▄▄▄▄ ██ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+	               ▀▀                
 EOF
-echo -e "\033[1;36m\n"
+}
 
-read -p "   ▶▶▶ DIGITE O ALVO:  " ALVO
-read -p "   ▶▶▶ DIGITE A WORDLIST:  " WORDLIST
-read -p "   ▶▶▶ DIGITE A EXTENSÃO:  " EXT
+# Script dentro de uma função
+script() {
+	echo -e "${BLD}${BLU}"
 
-echo -e "\n\033[1;39m   ▶▶▶ PROCURANDO DIRETÓRIOS \033[0m\n"
-for PALAVRA in $(cat $WORDLIST)
-do
-testar "$ALVO/$PALAVRA/"
-DIR=$(curl -s -H "User-Agent: Mozilla/5.0" -o /dev/null -w "%{http_code}" "$ALVO/$PALAVRA/")
-if [ "$DIR" == "200" ]
-then
-limpar
-echo "$ALVO/$PALAVRA/" | tee -a hbf_$ALVO
-fi
-done
-limpar
+	read -p "   ▶▶▶ DIGITE O ALVO:  " ALVO
+	read -p "   ▶▶▶ DIGITE A WORDLIST:  " WORDLIST
+	read -p "   ▶▶▶ DIGITE A EXTENSÃO:  " EXT
 
-echo -e "\n\033[1;39m   ▶▶▶ PROCURANDO ARQUIVOS \033[0m\n"
-for PALAVRA in $(cat $WORDLIST)
-do
-testar "$ALVO/$PALAVRA"
-FILE=$(curl -s -H "User-Agent: Mozilla/5.0" -o /dev/null -w "%{http_code}" "$ALVO/$PALAVRA")
-if [ "$FILE" == "200" ]
-then
-limpar
-echo "$ALVO/$PALAVRA" | tee -a hbf_$ALVO
-fi
-done
-limpar
+	echo -e "\n${BLD}${STD}   ▶▶▶ PROCURANDO DIRETÓRIOS ${RST}\n"
+	for PALAVRA in $(cat $WORDLIST); do
+		testar "$ALVO/$PALAVRA/"
+		DIR=$(curl -s -H "User-Agent: Mozilla" -o /dev/null -w "%{http_code}" "$ALVO/$PALAVRA/")
+		if [[ "$DIR" == "200" ]]; then
+			limpar
+			echo "$ALVO/$PALAVRA/" | tee -a hbf_$ALVO
+		fi
+	done
+	limpar
 
-echo -e "\n\033[1;39m   ▶▶▶ PROCURANDO ARQUIVOS .$EXT \033[0m\n"
-for PALAVRA in $(cat $WORDLIST)
-do
-testar "$ALVO/$PALAVRA.$EXT"
-FILE=$(curl -s -H "User-Agent: Mozilla/5.0" -o /dev/null -w "%{http_code}" "$ALVO/$PALAVRA.$EXT")
-if [ "$FILE" == "200" ]
-then
-limpar
-echo "$ALVO/$PALAVRA.$EXT" | tee -a hbf_$ALVO
-fi
-done
-limpar
+	echo -e "\n${BLD}${STD}   ▶▶▶ PROCURANDO ARQUIVOS ${RST}\n"
+	for PALAVRA in $(cat $WORDLIST); do
+		testar "$ALVO/$PALAVRA"
+		FILE=$(curl -s -H "User-Agent: Mozilla" -o /dev/null -w "%{http_code}" "$ALVO/$PALAVRA")
+		if [[ "$FILE" == "200" ]]; then
+			limpar
+			echo "$ALVO/$PALAVRA" | tee -a hbf_$ALVO
+		fi
+	done
+	limpar
 
-echo -e "\n\033[1;36m   ▶▶▶ PRONTO \033[0m\n"
-echo -e "\033[1;39m      ▶▶▶ RESULTADO SALVO EM:  \033[0mhbf_$ALVO\033[1;36m\n"
+	echo -e "\n${BLD}${STD}   ▶▶▶ PROCURANDO ARQUIVOS .$EXT ${RST}\n"
+	for PALAVRA in $(cat $WORDLIST); do
+		testar "$ALVO/$PALAVRA.$EXT"
+		FILE=$(curl -s -H "User-Agent: Mozilla" -o /dev/null -w "%{http_code}" "$ALVO/$PALAVRA.$EXT")
+		if [[ "$FILE" == "200" ]]; then
+			limpar
+			echo "$ALVO/$PALAVRA.$EXT" | tee -a hbf_$ALVO
+		fi
+	done
+	limpar
 
-read -p "   ▶▶▶ EXECUTAR NOVAMENTE? [y/N]  " RESPOSTA
-if [ "$RESPOSTA" != "y" ]
-then
-	echo -e "\n\033[1;31m   ▶▶▶ ENCERRADO \033[1;36m\n"
-else
-	./$0
-fi
+	echo -e "\n${BLD}${BLU}   ▶▶▶ PRONTO ${RST}\n"
+	echo -e "${BLD}${STD}      ▶▶▶ RESULTADO SALVO EM:  ${RST}hbf_$ALVO${BLD}${BLU}\n"
+
+	read -p "   ▶▶▶ EXECUTAR NOVAMENTE? [y/N]  " RESPOSTA
+	if [[ "$RESPOSTA" != "y" ]]; then
+		exit 0
+	else
+		script
+	fi
+}
+
+header
+script
