a6444415177dda796f31a5c094d6c2e311677c83
Єpke <gujerke@gmail.com>
2026-02-04 15:52:24 -0300

    Update htmlhostparser.sh

--- htmlhostparser.sh
+++ htmlhostparser.sh
@@ -1,47 +1,76 @@
 #!/bin/bash
 # https://github.com/e-pke/Bash-Scripts
 
-echo -e "\n\033[1;39m"
-clear
-cat << EOF
-  ┌────────────────────────────────┐
-  │        HTML HOST PARSER        │
-  └────────────────────────────────┘
+# Estilos
+BLD="\033[1m"
+RST="\033[0m"
 
-       ▄▄▄▄▄▄       ▄▄▄▄      by 
-      ██▀▀▀▀▀█       ██          
-      ██▄▄▄▄ ▀ ██▀█▄ ██ ▄█▀▄█▀█▄ 
-      ██▀▀▀▀ ▄ ██ ██ ████  ██▄█▀ 
-      ▀██████▀ ██▄█▀▄██ ▀█▄▀█▄▄■ 
-     ▄▄▄▄▄▄▄▄▄ ██ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-               ▀▀                
+# Cores
+RED="\033[31m"
+BLU="\033[36m"
+STD="\033[39m"
+
+# Função que limpa arquivos temporários
+temporario() {
+	rm PAGINA 2>/dev/null
+	rm LISTA 2>/dev/null
+}
+
+# Função para encerrar o script mesmo com Ctrl+C
+encerrar() {
+	echo -e "\n\n${BLD}${RED}   ▶▶▶ ENCERRADO ${BLD}${BLU}"
+}
+trap encerrar exit
+
+# Função cabeçalho
+header() {
+	echo -e "\n${BLD}${STD}"
+	cat <<- EOF
+	  ┌────────────────────────────────┐
+	  │        HTML HOST PARSER        │
+	  └────────────────────────────────┘
+	
+	       ▄▄▄▄▄▄       ▄▄▄▄      by 
+	      ██▀▀▀▀▀█       ██          
+	      ██▄▄▄▄ ▀ ██▀█▄ ██ ▄█▀▄█▀█▄ 
+	      ██▀▀▀▀ ▄ ██ ██ ████  ██▄█▀ 
+	      ▀██████▀ ██▄█▀▄██ ▀█▄▀█▄▄■ 
+	     ▄▄▄▄▄▄▄▄▄ ██ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+	               ▀▀                
 EOF
-echo -e "\033[1;36m\n"
+}
 
-read -p "   ▶▶▶ DIGITE O ALVO:  " ALVO
+# Script dentro de uma função
+script() {
+	echo -e "${BLD}${BLU}"
+	temporario
+	
+	read -p "   ▶▶▶ DIGITE O ALVO:  " ALVO
 
-echo -e "\n\033[1;39m   ▶▶▶ ACESSANDO PÁGINA \n"
-wget --user-agent="Mozilla/5.0" -O "PAGINA" "$ALVO" 2>/dev/null 
+	echo -e "\n${BLD}${STD}   ▶▶▶ ACESSANDO PÁGINA \n"
+	wget --user-agent="Mozilla/5.0" -O "PAGINA" "$ALVO" 2>/dev/null 
 
-echo -e "\033[1;39m   ▶▶▶ BUSCANDO HOSTS NA PÁGINA \033[0m\n"
-grep href PAGINA | grep http | cut -d "/" -f3 | cut -d '"' -f1  | grep "\." | grep -v " " > LISTA
-cat LISTA
+	echo -e "${BLD}${STD}   ▶▶▶ BUSCANDO HOSTS NA PÁGINA ${RST}\n"
+	grep href PAGINA | grep http | cut -d "/" -f3 | cut -d '"' -f1  | grep "\." | grep -v " " > LISTA
+	cat LISTA
 
-echo -e "\n\033[1;39m   ▶▶▶ RESOLVENDO HOSTS ENCONTRADOS \033[0m\n"
-for URL in $(cat LISTA)
-do
-host "$URL" | grep " has address " | tee -a hhp_$ALVO
-done
+	echo -e "\n${BLD}${STD}   ▶▶▶ RESOLVENDO HOSTS ENCONTRADOS ${RST}\n"
+	for URL in $(cat LISTA); do
+		host "$URL" | grep " has address " | tee -a hhp_$ALVO
+	done
 
-echo -e "\n\033[1;36m   ▶▶▶ PRONTO \033[0m\n"
-echo -e "\033[1;39m      ▶▶▶ RESULTADO SALVO EM:  \033[0mhhp_$ALVO\033[1;36m\n"
-rm PAGINA
-rm LISTA
+	echo -e "\n${BLD}${BLU}   ▶▶▶ PRONTO ${RST}\n"
+	echo -e "${BLD}${STD}      ▶▶▶ RESULTADO SALVO EM:  ${RST}hhp_$ALVO${BLD}${BLU}\n"
 
-read -p "   ▶▶▶ EXECUTAR NOVAMENTE? [y/N]  " RESPOSTA
-if [ "$RESPOSTA" != "y" ]
-then
-	echo -e "\n\033[1;31m   ▶▶▶ ENCERRADO \033[1;36m\n"
-else
-	./$0
-fi
+	temporario
+	
+	read -p "   ▶▶▶ EXECUTAR NOVAMENTE? [y/N]  " RESPOSTA
+	if [[ "$RESPOSTA" != "y" ]]; then
+		exit 0
+	else
+		script
+	fi
+}
+
+header
+script
