b3dc6cb3828a0f4334d261aadb9cd041d898d7bc
Єpke <gujerke@gmail.com>
2026-02-04 15:54:18 -0300

    Update subdomainbruteforce.sh

--- subdomainbruteforce.sh
+++ subdomainbruteforce.sh
@@ -1,49 +1,77 @@
 #!/bin/bash
 # https://github.com/e-pke/Bash-Scripts
 
-testar() { printf "\r      ▶▶▶ TESTANDO:  %-50s" "$1" >&2; }
-limpar() { printf "\r\033[K" >&2; }
+# Estilos
+BLD="\033[1m"
+RST="\033[0m"
 
-echo -e "\n\033[1;39m"
-clear
-cat << EOF
-  ┌────────────────────────────────┐
-  │     SUBDOMAIN BRUTE FORCE      │
-  └────────────────────────────────┘
+# Cores
+RED="\033[31m"
+BLU="\033[36m"
+STD="\033[39m"
 
-       ▄▄▄▄▄▄       ▄▄▄▄      by 
-      ██▀▀▀▀▀█       ██          
-      ██▄▄▄▄ ▀ ██▀█▄ ██ ▄█▀▄█▀█▄ 
-      ██▀▀▀▀ ▄ ██ ██ ████  ██▄█▀ 
-      ▀██████▀ ██▄█▀▄██ ▀█▄▀█▄▄■ 
-     ▄▄▄▄▄▄▄▄▄ ██ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-               ▀▀                
+# Função para mostrar o que está sendo testado
+testar() {
+	printf "\r      ▶▶▶ TESTANDO:  %-50s" "$1" >&2
+}
+
+# Função para limpar a linha após o teste
+limpar() {
+	printf "\r\033[K" >&2
+}
+
+# Função para encerrar o script mesmo com Ctrl+C
+encerrar() {
+	echo -e "\n\n${BLD}${RED}   ▶▶▶ ENCERRADO ${BLD}${BLU}"
+}
+trap encerrar exit
+
+# Função cabeçalho
+header() {
+	echo -e "\n${BLD}${STD}"
+	cat <<- EOF
+	  ┌────────────────────────────────┐
+	  │     SUBDOMAIN BRUTE FORCE      │
+	  └────────────────────────────────┘
+
+	       ▄▄▄▄▄▄       ▄▄▄▄      by 
+	      ██▀▀▀▀▀█       ██          
+	      ██▄▄▄▄ ▀ ██▀█▄ ██ ▄█▀▄█▀█▄ 
+	      ██▀▀▀▀ ▄ ██ ██ ████  ██▄█▀ 
+	      ▀██████▀ ██▄█▀▄██ ▀█▄▀█▄▄■ 
+	     ▄▄▄▄▄▄▄▄▄ ██ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+	               ▀▀                
 EOF
-echo -e "\033[1;36m\n"
+}
 
-read -p "   ▶▶▶ DIGITE O ALVO:  " ALVO
-read -p "   ▶▶▶ DIGITE A WORDLIST:  " WORDLIST
+# Script dentro de uma função
+script() {
+	echo -e "${BLD}${BLU}"
 
-echo -e "\n\033[1;39m   ▶▶▶ TESTANDO SUBDOMÍNIOS \033[0m\n"
-for PALAVRA in $(cat $WORDLIST)
-do
-testar "$PALAVRA.$ALVO"
-DNS=$(host "$PALAVRA.$ALVO" | egrep -v "not found")
-if [ "$DNS" != "" ]
-then
-limpar
-echo "$DNS" | tee -a sbf_$ALVO
-fi
-done
-limpar
+	read -p "   ▶▶▶ DIGITE O ALVO:  " ALVO
+	read -p "   ▶▶▶ DIGITE A WORDLIST:  " WORDLIST
 
-echo -e "\n\033[1;36m   ▶▶▶ PRONTO \033[0m\n"
-echo -e "\033[1;39m      ▶▶▶ RESULTADO SALVO EM:  \033[0msbf_$ALVO\033[1;36m\n"
+	echo -e "\n${BLD}${STD}   ▶▶▶ TESTANDO SUBDOMÍNIOS ${RST}\n"
+	for PALAVRA in $(cat $WORDLIST); do
+		testar "$PALAVRA.$ALVO"
+		DNS=$(host "$PALAVRA.$ALVO" | egrep -v "not found")
+		if [[ -n "$DNS" ]]; then
+			limpar
+			echo "$DNS" | tee -a sbf_$ALVO
+		fi
+	done
+	limpar
 
-read -p "   ▶▶▶ EXECUTAR NOVAMENTE? [y/N]  " RESPOSTA
-if [ "$RESPOSTA" != "y" ]
-then
-	echo -e "\n\033[1;31m   ▶▶▶ ENCERRADO \033[1;36m\n"
-else
-	./$0
-fi
+	echo -e "\n${BLD}${BLU}   ▶▶▶ PRONTO ${RST}\n"
+	echo -e "${BLD}${STD}      ▶▶▶ RESULTADO SALVO EM:  ${RST}sbf_$ALVO${BLD}${BLU}\n"
+
+	read -p "   ▶▶▶ EXECUTAR NOVAMENTE? [y/N]  " RESPOSTA
+	if [[ "$RESPOSTA" != "y" ]]; then
+		exit 0
+	else
+		script
+	fi
+}
+
+header
+script
